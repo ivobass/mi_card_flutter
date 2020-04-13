b9280e67bbb05f74599f22e61461f44fcbea3f49
Ivo Baptista <ivobass@gmail.com>
2020-04-13 16:05:17 +0100

    primeira parte
    primeira parte

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Row(
+             crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+               width: 30.0,
+                  // margin: EdgeInsets.only(left: 30.0),
+                  // padding: EdgeInsets.all(20.0),
+                  color: Colors.white,
+                  child: Text('Container 1'),
+                ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Container(
+                  
+                  // margin: EdgeInsets.only(left: 30.0),
+                  // padding: EdgeInsets.all(20.0),
+                  color: Colors.blue,
+                  child: Text('Container 2'),
+                ),
+                Container(
+                
+                  // margin: EdgeInsets.only(left: 30.0),
+                  // padding: EdgeInsets.all(20.0),
+                  color: Colors.red,
+                  child: Text('Container 3'),
+                ),
+            
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
